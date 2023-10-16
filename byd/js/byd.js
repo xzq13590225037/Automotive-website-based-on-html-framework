fd685f8e1cf8a263fbcbeaa5958b3c879de343d9
xzq13590225037 <1211879473@qq.com>
2023-10-16 18:10:08 +0800

    Automotive html

---
+++ byd/js/byd.js
@@ -0,0 +1,483 @@
+// 柱状图1
+(function() {
+  // 1实例化对象
+  var myChart = echarts.init(document.querySelector(".bar .chart"));
+  // 2. 指定配置项和数据
+  var option = {
+    color: ["#2f89cf"],
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        // 坐标轴指示器，坐标轴触发有效
+        type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+      }
+    },
+    // 修改图表的大小
+    grid: {
+      left: "0%",
+      top: "10px",
+      right: "0%",
+      bottom: "4%",
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: "category",
+        data: [
+          "海豚",
+          "秦Pro新能源",
+          "汉",
+          "元Pro",
+          "宋Pro",
+          "唐新能源",
+          "宋Plus"
+        ],
+        axisTick: {
+          alignWithLabel: true
+        },
+        // 修改刻度标签 相关样式
+        axisLabel: {
+          color: "#00008B",
+          fontSize: "12"
+        },
+        // 不显示x坐标轴的样式
+        axisLine: {
+          show: false
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: "value",
+        // 修改刻度标签 相关样式
+        axisLabel: {
+          color: "rgba(255,255,255,.6) ",
+          fontSize: 12
+        },
+        // y轴的线条改为了 2像素
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+            width: 2
+          }
+        },
+        // y轴分割线的颜色
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: "售价(万元)",
+        type: "bar",
+        barWidth: "35%",
+        data: [13.08, 29.99, 32.98, 14.38, 11.28, 33.98, 14.38],
+        itemStyle: {
+          // 修改柱子圆角
+          barBorderRadius: 5
+        }
+      }
+    ]
+  };
+  // 3. 把配置项给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+})();
+// 柱状图2
+(function() {
+  var myColor = ["#1089E7", "#F57474", "#56D0E3", "#F8B448", "#8B78F6"];
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector(".bar2 .chart"));
+  // 2. 指定配置和数据
+  var option = {
+    grid: {
+      top: "10%",
+      left: "22%",
+      bottom: "10%"
+      // containLabel: true
+    },
+    // 不显示x轴的相关信息
+    xAxis: {
+      show: false
+    },
+    yAxis: [
+      {
+        type: "category",
+        inverse: true,
+        data: ["比亚迪海豚", "比亚迪元Pro", "比亚迪宋Plus", "比亚迪唐", "比亚迪E5"],
+        // 不显示y轴的线
+        axisLine: {
+          show: false
+        },
+        // 不显示刻度
+        axisTick: {
+          show: false
+        },
+        // 把刻度标签里面的文字颜色设置为白色
+        axisLabel: {
+          color: "#00BFFF"
+        }
+      },
+      {
+        data: [96, 95, 96, 96, 85],
+        inverse: true,
+        // 不显示y轴的线
+        axisLine: {
+          show: false
+        },
+        // 不显示刻度
+        axisTick: {
+          show: false
+        },
+        // 把刻度标签里面的文字颜色设置为白色
+        axisLabel: {
+          color: "#FFFF00"
+        }
+      }
+    ],
+    series: [
+      {
+        name: "条",
+        type: "bar",
+        data: [85, 90, 80, 87, 89],
+        yAxisIndex: 0,
+        // 修改第一组柱子的圆角
+        itemStyle: {
+          barBorderRadius: 20,
+          // 此时的color 可以修改柱子的颜色
+          color: function(params) {
+            // params 传进来的是柱子对象
+            // console.log(params);
+            // dataIndex 是当前柱子的索引号
+            return myColor[params.dataIndex];
+          }
+        },
+        // 柱子之间的距离
+        barCategoryGap: 50,
+        //柱子的宽度
+        barWidth: 10,
+        // 显示柱子内的文字
+        label: {
+          show: true,
+          position: "inside",
+          // {c} 会自动的解析为 数据  data里面的数据
+          formatter: "{c}%"
+        }
+      },
+      {
+        name: "框",
+        type: "bar",
+        barCategoryGap: 50,
+        barWidth: 15,
+        yAxisIndex: 1,
+        data: [100, 100, 100, 100, 100],
+        itemStyle: {
+          color: "none",
+          borderColor: "#00c1de",
+          borderWidth: 3,
+          barBorderRadius: 15
+        }
+      }
+    ]
+  };
+
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+})();
+// 折线图1
+(function() {
+  var yearData = [
+    {
+      year: "2020", // 年份
+      data: [
+        // 两个数组是因为有两条线
+        [2.83,2.45, 2.37, 2.44, 2.77, 2.89],
+        [0.42,0.57,0.27,0.49,0.81,0.51]
+      ]
+    },
+    {
+      year: "2021", // 年份
+      data: [
+        // 两个数组是因为有两条线
+        [6.04,6.24,5.40,5.48,5.20,5.43],
+        [1.19,1.04,0.85,1.06,1.01,0.88]
+      ]
+    }
+  ];
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector(".line .chart"));
+  // 2.指定配置
+  var option = {
+    // 通过这个color修改两条线的颜色
+    color: ["#00f2f1", "#ed3f35"],
+    tooltip: {
+      trigger: "axis"
+    },
+    legend: {
+      // 如果series 对象有name 值，则 legend可以不用写data
+      // 修改图例组件 文字颜色
+      textStyle: {
+        color: "#4c9bfd"
+      },
+      // 这个10% 必须加引号
+      right: "10%"
+    },
+    grid: {
+      top: "20%",
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      show: true, // 显示边框
+      borderColor: "#012f4a", // 边框颜色
+      containLabel: true // 包含刻度文字在内
+    },
+
+    xAxis: {
+      type: "category",
+      boundaryGap: false,
+      data: [
+        "1月",
+        "2月",
+        "3月",
+        "4月",
+        "5月",
+        "6月"
+      ],
+      axisTick: {
+        show: false // 去除刻度线
+      },
+      axisLabel: {
+        color: "#4c9bfd" // 文本颜色
+      },
+      axisLine: {
+        show: false // 去除轴线
+      }
+    },
+    yAxis: {
+      type: "value",
+      axisTick: {
+        show: false // 去除刻度线
+      },
+      axisLabel: {
+        color: "#4c9bfd" // 文本颜色
+      },
+      axisLine: {
+        show: false // 去除轴线
+      },
+      splitLine: {
+        lineStyle: {
+          color: "#012f4a" // 分割线颜色
+        }
+      }
+    },
+    series: [
+      {
+        name: "比亚迪朝代系列",
+        type: "line",
+        // true 可以让我们的折线显示带有弧度
+        smooth: true,
+        data: yearData[0].data[0]
+      },
+      {
+        name: "比亚迪其他系列",
+        type: "line",
+        smooth: true,
+        data: yearData[0].data[1]
+      }
+    ]
+  };
+
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+
+  // 5.点击切换效果
+  $(".line h2").on("click", "a", function() {
+    // alert(1);
+    // console.log($(this).index());
+    // 点击 a 之后 根据当前a的索引号 找到对应的 yearData的相关对象
+    // console.log(yearData[$(this).index()]);
+    var obj = yearData[$(this).index()];
+    option.series[0].data = obj.data[0];
+    option.series[1].data = obj.data[1];
+    // 需要重新渲染
+    myChart.setOption(option);
+  });
+})();
+// 折线图2 
+(function() {
+  var chartDom = document.getElementById('zhe2');
+  var myChart = echarts.init(chartDom);
+  var option;
+
+option = {
+  title:{
+    text:"比亚迪汽车市值(亿美元)"
+  },
+  xAxis: {
+    type: 'category',
+    data: ['2015', '2016', '2017', '2018', '2019', '2020', '2021']
+  },
+  grid: {
+    top: "20%",
+    left: "3%",
+    right: "4%",
+    bottom: "3%",
+    show: true, // 显示边框
+    borderColor: "#012f4a", // 边框颜色
+    containLabel: true // 包含刻度文字在内
+  },
+  tooltip: {
+    trigger: "axis"
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [201, 196, 260, 202, 167, 162, 701],
+      type: 'line',
+      smooth: true
+    }
+  ]
+};
+
+option && myChart.setOption(option);
+})();
+// 饼形图1
+(function() {
+  // 1. 实例化对象
+  var myChart = echarts.init(document.querySelector(".pie .chart"));
+  // 2.指定配置
+  var option = {
+    color: ["#065aab", "#066eab", "#0682ab", "#0696ab", "#06a0ab"],
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+
+    legend: {
+      bottom: "0%",
+      // 修改小图标的大小
+      itemWidth: 10,
+      itemHeight: 10,
+      // 修改图例组件的文字为 12px
+      textStyle: {
+        color: "rgbc(255,255,255,.5)",
+        fontSize: "12"
+      }
+    },
+    series: [
+      {
+        name: "类型分布",
+        type: "pie",
+        // 这个radius可以修改饼形图的大小
+        // radius 第一个值是内圆的半径 第二个值是外圆的半径
+        radius: ["40%", "60%"],
+        center: ["50%", "45%"],
+        avoidLabelOverlap: false,
+        // 图形上的文字
+        label: {
+          show: false,
+          position: "center"
+        },
+        // 链接文字和图形的线是否显示
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1, name: "比亚迪元Plus" },
+          { value: 4, name: "比亚迪海豚" },
+          { value: 2, name: "比亚迪唐" },
+          { value: 2, name: "比亚迪宋" },
+          { value: 1, name: "比亚迪E5" }
+        ]
+      }
+    ]
+  };
+
+  // 3. 把配置给实例对象
+  myChart.setOption(option);
+  // 4. 让图表跟随屏幕自动的去适应
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+})();
+
+// 饼形图2 
+(function() {
+  var myChart = echarts.init(document.querySelector(".pie2 .chart"));
+  var option = {
+    color: [
+      "#006cff",
+      "#60cda0",
+      "#ed8884",
+      "#ff9f7f",
+      "#0096ff",
+      "#9fe6b8",
+      "#32c5e9",
+      "#1d9dff"
+    ],
+    tooltip: {
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+      bottom: "0%",
+      itemWidth: 10,
+      itemHeight: 10,
+      textStyle: {
+        color: "rgbc(255,255,255,.5)",
+        fontSize: "12"
+      }
+    },
+    series: [
+      {
+        name: "品牌",
+        type: "pie",
+        radius: ["10%", "70%"],
+        center: ["50%", "50%"],
+        roseType: "radius",
+        // 图形的文字标签
+        label: {
+          fontSize: 10
+        },
+        // 链接图形和文字的线条
+        labelLine: {
+          // length 链接图形的线条
+          length: 6,
+          // length2 链接文字的线条
+          length2: 8
+        },
+        data: [
+          { value: 248492, name: "大众" },
+          { value: 112021, name: "丰田" },
+          { value: 84622, name: "宝马" },
+          { value: 76039, name: "奔驰" },
+          { value: 96786, name: "现代" },
+          { value: 47165, name: "标致" },
+          { value: 38873, name: "斯柯达" },
+          { value: 110437, name: "雷诺" }
+        ]
+      }
+    ]
+  };
+  myChart.setOption(option);
+  // 监听浏览器缩放，图表对象调用缩放resize函数
+  window.addEventListener("resize", function() {
+    myChart.resize();
+  });
+})();
